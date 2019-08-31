e2608d70b2f8747da45360386be385737b294c0a
Andrew Wang <azw7@cornell.edu>
2019-08-30 17:00:31 -0700

    speed test

--- reinforce.py
+++ reinforce.py
@@ -5,7 +5,7 @@ import sys
 import queue
 
 n_workers = 4
-game_batch_size = 64
+game_batch_size = 128
 max_recent_opps = 10000
 pool_update_dur = 64
 
@@ -20,26 +20,65 @@ def train(model, opt, criterion, boards, metas, actions, reward):
     return loss
 
 def run_game(n_games, queue, model, opp_model, epoch):
-    moves, states, rewards = [], [], []
-    for game_num in range(n_games):
-        board = chess.Board()
-        my_side = (epoch + game_num) % 2 == 0
-        n_moves = 0
-        while not board.is_game_over():
-            if board.turn == my_side:
-                move = choose_move(board, model, 0)
-                # record for replay
-                moves.append(move.uci())
-                states.append(board.fen())
-                n_moves += 1
-            else:
-                move = choose_move(board, opp_model, 0)
-            board.push(move)
+    moves = [[] for i in range(n_games)]
+    states = [[] for i in range(n_games)]
+    rewards = [[] for i in range(n_games)]
+    boards = [chess.Board() for i in range(n_games)]
+    n_done = 0
+    t = 0
 
-        reward = reward_for_side(board, my_side)
-        rewards += [reward]*n_moves
+    while n_done < n_games:
+        board_t, meta_t = states_to_tensor([board.fen() for board in boards])
+        board_t = board_t.type(torch.float)
+        meta_t = meta_t.type(torch.float)
+        if t % 2 == 0:
+            pred_w = model(board_t[:n_games//2], meta_t[:n_games//2])
+            pred_b = opp_model(board_t[n_games//2:], meta_t[n_games//2:])
+        else:
+            pred_b = model(board_t[n_games//2:], meta_t[n_games//2:])
+            pred_w = opp_model(board_t[:n_games//2], meta_t[:n_games//2])
+        pred = torch.cat((pred_w, pred_b), dim=0)
 
-    queue.put((moves, states, rewards))
+        for n, board in enumerate(boards):
+            if not board.is_game_over():
+                legal_moves = list(board.legal_moves)
+                valid_idxs = [move_to_action_idx(move) for move in legal_moves]
+                pred_n = pred[n][valid_idxs]
+                actions = torch.distributions.Categorical(logits=pred_n)
+                move = legal_moves[actions.sample().item()]
+                if move.promotion is not None:
+                    move.promotion = 5
+                if (n < n_games//2) == (t % 2 == 0):# TODO
+                    moves[n].append(move.uci())
+                    states[n].append(board.fen())
+                board.push(move)
+                if board.is_game_over():
+                    n_done += 1
+                    reward = reward_for_side(board, n < n_games//2)
+                    rewards[n] += [reward]*len(moves[n])
+
+                    queue.put((moves[n], states[n], rewards[n]))
+        t += 1
+
+    #for game_num in range(n_games):
+    #    board = chess.Board()
+    #    my_side = (epoch + game_num) % 2 == 0
+    #    n_moves = 0
+    #    while not board.is_game_over():
+    #        if board.turn == my_side:
+    #            move = choose_move(board, model, 0)
+    #            # record for replay
+    #            moves.append(move.uci())
+    #            states.append(board.fen())
+    #            n_moves += 1
+    #        else:
+    #            move = choose_move(board, opp_model, 0)
+    #        board.push(move)
+
+    #    reward = reward_for_side(board, my_side)
+    #    rewards += [reward]*n_moves
+
+    #queue.put((moves, states, rewards))
 
 if __name__ == "__main__":
     use_mp = False
@@ -63,7 +102,7 @@ if __name__ == "__main__":
     opp_model_pool = []
 
     #opt = optim.Adam(model.parameters(), lr=1e-4)
-    opt = optim.SGD(model.parameters(), lr=1e-4)
+    opt = optim.SGD(model.parameters(), lr=1e-5)
     criterion = nn.NLLLoss(reduction="none")
 
     for epoch in range(10000):
